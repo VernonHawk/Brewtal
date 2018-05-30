08545e756ec87b7419d1da93b1ddb6cb89da0e60
Igor Morenec <Igor.Morenec@gmail.com>
2018-05-30 20:30:42 +0300

    Implemented basic web-server, which sends index.html for every request. Added aws-sdk to dependencies for future.

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
 "use strict";
+
+const express    = require("express");
+const bodyParser = require("body-parser");
+const helmet     = require("helmet");
+const path       = require("path");
+
+const app = express();
+
+const CLIENT_PATH = path.resolve(`${__dirname}/client`);
+
+function mapRoutes() {
+    app.get("*", (req, res) => res.sendFile(`${CLIENT_PATH}/index.html`));
+}
+
+function startServer() {
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+        require("dotenv").config();
+    }
+
+    app.use(express.static(CLIENT_PATH));
+    
+    app.use(helmet());
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    mapRoutes();
+
+    app.set("port", process.env.PORT || 3000);
+    
+    app.listen(app.get("port"), () => console.log(`App listening on port ${app.get("port")}`)); 
+}
+
+startServer();
+
+module.exports.start = startServer;
