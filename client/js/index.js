1fb9b90a843803e807e6f0059f269f98ea115316
Annabell <Ts.Nyutka@gmail.com>
2018-06-09 18:58:45 +0300

    fixed bugs

--- client/js/index.js
+++ client/js/index.js
@@ -1,15 +1,36 @@
 'use strict';
 
-let currentItemCount = 0;
 let currentItemPage = 0;
 let availableRowCount;
+let ingredients;
 
-const itemPerRow = 2;
+class Ingredients {
+    constructor(itemPerRow) {
+        this._itemPerRow = itemPerRow;
+        this._currentItemCount = 0;
+    }
+    get itemPerRow() {
+        return this._itemPerRow;
+    }
+    set itemPerRow(value) {
+        this._itemPerRow = value;
+    }
+    get currentItemCount() {
+        return this._currentItemCount;
+    }
+    set currentItemCount(value) {
+        this._currentItemCount = value;
+    }
+    get currentRowCount() {
+        return this._currentItemCount / this._itemPerRow;
+    }
+
+    decRow() {
+        this._currentItemCount -= this._itemPerRow;
+    }
+}
+
 const ingredientList = [{
-    name: 'Stone',
-    description: 'Sgdfg hghgfd ggfdsgfh gfgf dgfg gfdgf gfdgf gfdgdf',
-    url: '../img/svg/stone.svg'
-}, {
     name: 'Stone1',
     description: 'Sgdfg hghgfd ggfdsgfh gfgf dgfg gfdgf gfdgf gfdgdf',
     url: '../img/svg/stone.svg'
@@ -23,6 +44,10 @@ const ingredientList = [{
     url: '../img/svg/stone.svg'
 }, {
     name: 'Stone4',
+    description: 'Sgdfg hghgfd ggfdsgfh gfgf dgfg gfdgf gfdgf gfdgdf',
+    url: '../img/svg/stone.svg'
+}, {
+    name: 'Stone5',
     description: 'Sgdfg hghgfd ggfdsgfh gfgf',
     url: '../img/svg/stone.svg'
 }, {
@@ -44,21 +69,21 @@ const ingredientList = [{
 }];
 
 const ingredientRowItem = (item, index, arr) => {
-    let isEven = index % itemPerRow == 0;
+    let isEven = index % ingredients.itemPerRow == 0;
     let nextIndex = index + 1;
 
     return `
         ${index == 0 || isEven ? '<div class="row valign-wrapper">' : ''}
         <div class="col ${isEven ? 'offset-s2' : ''} s2 tooltipped" data-index=${index} data-position="top" data-tooltip="I'm ${item.name}. Drag me!">
             <a id="${item.name}" href="#" class="col s12 clickable">
-                    <img src="${item.url}" alt=""/>
+                <img src="${item.url}" alt=""/>
             </a>
         </div>
         <div class="col ${nextIndex == arr.length && isEven ? 's8' : 's3'}">
             <h5>${item.name}</h5>
             <p class="truncate">${item.description}</p>
         </div>
-        ${nextIndex == arr.length || nextIndex % itemPerRow == 0 ? `</div>` : `${ingredientRowItem(arr[nextIndex], nextIndex, arr)}`}`;
+        ${nextIndex == arr.length || nextIndex % ingredients.itemPerRow == 0 ? `</div>` : `${ingredientRowItem(arr[nextIndex], nextIndex, arr)}`}`;
 };
 /*
                 <object type="image/svg+xml" data="${item.url}">
@@ -72,13 +97,9 @@ const getChangesCount = () => {
     let rowHeight = container.find('.row:first-child').outerHeight(true);
     let parentHeight = $('#ingredients').height();
 
-    // console.log("r:" + $('#ingredients > div:first-child').outerHeight(true));
-
-    console.log(Math.floor(parentHeight / rowHeight) - 1, container.find('.row').length, Math.ceil(ingredientList.length / itemPerRow),
-        Math.floor(parentHeight / rowHeight) - container.find('.row').length - 1
-    );
-    // console.log(currentItemCount);//currentItemCount / itemPerRow
-    //console.log(availableRowCount);//currentItemCount / itemPerRow
+    // console.log(Math.floor(parentHeight / rowHeight) - 1, container.find('.row').length, Math.ceil(ingredientList.length / ingredients.itemPerRow),
+    //     Math.floor(parentHeight / rowHeight) - container.find('.row').length - 1
+    // );
     return Math.floor(parentHeight / rowHeight) - container.find('.row').length - 1;
 };
 
@@ -107,64 +128,73 @@ $(document).ready(() => {
         </div>
     `);
 
+    ingredients = new Ingredients(2);
     loadIngredients(ingredientList);
 });
 
-const loadIngredients = (items, startPos = 0) => {
-    let container = $('#ingredients > div:first-child');
+const loadIngredients = async (items, startPos = 0, emptied = true) => {
+    return new Promise(resolve => {
+        let container = $('#ingredients > div:first-child');
 
-    const updatePageButtons = () => {
-        let firstIndex = container.find('.row:first > .tooltipped:first').data('index');
-        let lastIndex = container.find('.row:last > .tooltipped:last').data('index');
+        const updatePageButtons = () => {
+            let firstIndex = container.find('.row:first > .tooltipped:first').data('index');
+            let lastIndex = container.find('.row:last > .tooltipped:last').data('index');
 
-        $('#pagebar .clickable:first').toggleClass('disable', firstIndex == 0);
-        $('#pagebar .clickable:last').toggleClass('disable', lastIndex + 1 >= ingredientList.length);
-    };
-    const updatePageBar = () => {
-        let rowCount = container.find('.row').length;
-        let maxRowCount = Math.ceil(ingredientList.length / itemPerRow);
+            $('#pagebar .clickable:first').toggleClass('disable', firstIndex == 0);
+            $('#pagebar .clickable:last').toggleClass('disable', lastIndex + 1 >= ingredientList.length);
+        };
+        const updatePageBar = () => {
+            let rowCount = container.find('.row').length;
+            let maxRowCount = Math.ceil(ingredientList.length / ingredients.itemPerRow);
 
-        $('#ingredients > div:last-child').toggleClass('hidden', rowCount >= maxRowCount);
-    };
+            $('#ingredients > div:last-child').toggleClass('hidden', rowCount >= maxRowCount);
+        };
 
-    container.empty();
-    for (let i = startPos, j = 0, rowHeight, parentHeight; i < items.length; i += itemPerRow, j++) {
-        $(ingredientRowItem(items[i], i, items))
-            .hide()
-            .appendTo(container)
-            .fadeIn(1000);
-        if (!j) {
-            rowHeight = container.find('.row:first-child').outerHeight(true);
-            parentHeight = $('#ingredients').height();
-            availableRowCount = Math.floor(parentHeight / rowHeight) - 1;
+        if (emptied) {
+            container.empty();
         }
-        if (j + 1 == availableRowCount) {
-            currentItemCount = availableRowCount * itemPerRow;
-            break;
+        for (let i = startPos, j = 0, rowHeight, parentHeight; i < items.length; i += ingredients.itemPerRow, j++) {
+            $(ingredientRowItem(items[i], i, items))
+                .hide()
+                .appendTo(container)
+                .fadeIn(1000);
+            if (!j) {
+                rowHeight = container.find('.row:first-child').outerHeight(true);
+                parentHeight = $('#ingredients').height();
+                availableRowCount = Math.floor(parentHeight / rowHeight) - 1;
+            }
+            if (j + 1 + (emptied ? 0 : ingredients.currentRowCount) == availableRowCount) {
+                console.log(startPos, j, emptied, ingredients.currentRowCount, availableRowCount);
+                ingredients.currentItemCount = availableRowCount * ingredients.itemPerRow;
+                break;
+            }
         }
-    }
-    container.find('.clickable').click(e => {
-        let item = e.target;
-        console.log(item.id);
-    });
-    if ($('#pagebar').length) {
-        updatePageButtons();
-    } else {
-        $('#ingredients').append($('<div>').load('parts/pagebar.html', () => {
-            $('#pagebar .clickable').click(e => {
-                let item = $(e.target);
-                if (!item.hasClass('disable')) {
-                    let firstIndex = container.find('.row:first > .tooltipped:first').data('index');
-                    let direction = item.data('direction');
-
-                    currentItemPage += direction;
-                    loadIngredients(ingredientList, firstIndex + direction * currentItemCount);
-                }
-            });
+        container.find('.clickable').click(e => {
+            let item = e.target;
+            console.log(item.id);
+        });
+        if ($('#pagebar').length) {
             updatePageButtons();
-        }));
-    }
-    updatePageBar();//--all browsers!
+        } else {
+            $('#ingredients').append($('<div>').load('parts/pagebar.html', () => {
+                $('#pagebar .clickable').click(e => {
+                    let item = $(e.target);
+
+                    if (!item.hasClass('disable')) {
+                        let direction = item.data('direction');
+                        let firstIndex = container.find('.row:first > .tooltipped:first').data('index');
+
+                        currentItemPage += direction;
+                        // if (direction == -1)
+                        //     console.log(firstIndex, direction, ingredients.currentItemCount);
+                        loadIngredients(ingredientList, firstIndex + direction * ingredients.currentItemCount);
+                    }
+                });
+                updatePageButtons();
+            }));
+        }
+        updatePageBar(); //--all browsers!
+    });
 };
 
 $(window).resize(() => {
@@ -173,16 +203,25 @@ $(window).resize(() => {
     if (changesCount) {
         let container = $('#ingredients > div:first-child');
 
+        console.log('changes: ' + changesCount);
         if (changesCount < 0) {
             while (changesCount != 0) {
-                console.log(-111);
-                container.find('.row:last-child').remove();
+                container.find('.row:last-child').fadeOut('fast', function () {
+                    $(this).remove();
+                    ingredients.decRow();
+                });
                 changesCount++;
             }
-            // console.log(1111);//delete
         } else {
-            //append
+            while (changesCount != 0) {
+                loadIngredients(ingredientList,
+                    container.find('.row:first > .tooltipped:first').data('index') +
+                    ingredients.currentItemCount,
+                    false)
+                .then(() => {ingredients.decRow();});
+
+                changesCount--;
+            }
         }
     }
-    // loadIngredients(ingredientList);
 });
