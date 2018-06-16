31cd53f49ca1c45166ff9ca4b37a6a19ecef0124
Igor Morenec <Igor.Morenec@gmail.com>
2018-06-16 17:05:57 +0300

    Implemented cocktail composition. Wrote few tests. TODO: Fix incorrect type error on some images

--- server/controllers/cocktailController.js
+++ server/controllers/cocktailController.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const sharp = require("sharp");
 const { s3 } = require("../awsConnect");
 
 /**
@@ -15,8 +16,208 @@ const { s3 } = require("../awsConnect");
  * 
  * @returns {Object<string> | Object<string, string>>} Response with cocktail image OR error
  */
-function getCocktail(req, res) {
-    res.status(501).json(req.query);
+async function getCocktail(req, res) {
+    // Validation
+    if (!validParams(req.query)) {
+        return res.status(400).json({ error: "Invalid parameters" });
+    }
+
+    const { layer1, layer2, layer3, layer4 } = req.query;
+    
+    const layers = [ layer1, layer2, layer3, layer4 ].filter(Boolean);
+
+    try {
+        // Get glass and layer images
+        const images = await getImages(layers);
+
+        // Convert images to appropriate shapes  
+        const [ glassShaped, ...layersShaped ] = await getShapedImages(images);
+        
+        // Compose images
+        const result = await composeImages({ glass: glassShaped, layers: layersShaped });
+
+        res.status(200).json({ data: dataToBase64(result) });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error });
+    }
 }
 
-module.exports = { getCocktail };
+/**
+ * Perform request parameters validation
+ * 
+ * @param {Object} params Request parameters
+ * @param {Object} params.layer1 Expected bottom layer
+ * @param {Object} params.layer2 Expected layer 2
+ * @param {Object} params.layer3 Expected layer 3
+ * @param {Object} params.layer4 Expected top layer
+ * 
+ * @returns {Boolean} Are parameters valid
+ */
+function validParams({ layer1, layer2, layer3, layer4, ...rest }) {
+    if (Object.keys(rest).length !== 0) {
+        return false;
+    }
+
+    const layers = [ layer1, layer2, layer3, layer4 ];
+
+    // Test for prohibited characters
+    const allowedRegexp = /^([a-z]|[A-Z])+(-([a-z]|[A-Z])+)*$/;
+
+    if (layers.some( el => !(allowedRegexp).test(el) )) {
+        return false;
+    }
+
+    // Test for holes
+    for (let i = 0; i < layers.length; i++) {
+        if (!layers[i]) {
+            return layers.slice(i).every( el => !el );
+        }
+    }
+
+    return true;
+}
+
+/**
+ * Get images for the glass and layers
+ * 
+ * @async
+ * 
+ * @param {String[]} ingredients Keys to fetch from S3
+ * 
+ * @returns {Promise<[]>} Promise of images array
+ */
+function getImages(ingredients) {
+    const getRequest = params => s3.getObject(params).promise();
+
+    const glassRequest = getRequest({ Bucket: process.env.BUCKET_OTHER, Key: "glass.svg" });
+
+    const ingredientsRequests = ingredients.map( 
+        id => getRequest({ Bucket: process.env.BUCKET_GLASS, Key: `${id}.svg` }));
+
+    return Promise.all([ glassRequest, ...ingredientsRequests ]);
+}
+
+/**
+ * Get shaped images
+ * 
+ * @async
+ * 
+ * @param {Object[]} images Array of images to shape
+ * 
+ * @returns {Promise<[]>} Promise of shaped images array
+ */
+function getShapedImages(images) {
+    const shapeOptions = [
+        { width: 250, height: 400 }, // glass
+        getImageShape({ width: 165, height: 120, rounded: true }), // bottom layer
+        getImageShape({ width: 167, height: 82 }), // layer 2
+        getImageShape({ width: 174, height: 73 }), // layer 3
+        getImageShape({ width: 178, height: 73 })  // top layer
+    ];
+    
+    const shapedImages = images.map( 
+        (image, i) => shapeImage({ image: image.Body, shape: shapeOptions[i] }) 
+    );
+    
+    return Promise.all(shapedImages);
+}
+
+/**
+ * Get image shape for shaping
+ * 
+ * @param {Object} params Function parameters
+ * @param {Number} width  Image width
+ * @param {Number} height Image height
+ * @param {Boolean} rounded Should image have round corners
+ * 
+ * @returns {Object<Number, Number, Buffer>} Generated image shape object 
+ */
+function getImageShape({ width, height, rounded }) {
+    const mask = Buffer.from(
+        `<svg style='opacity: 0.95'>
+            <rect x='0' y='0' ${rounded ? "rx='44' ry='26'" : ""} width='${width}' height='${height}' />
+        </svg>`);
+
+    return { width, height, mask };
+}
+
+/**
+ * Get shaped images (used in map)
+ * 
+ * @async
+ * 
+ * @param {Object} params Function parameters
+ * @param {Buffer|String} params.image Image data to shape
+ * @param {Object} params.shape Shape to apply to the image
+ * @param {Number} shape.width  Image new width
+ * @param {Number} shape.height Image new height
+ * @param {Buffer} shape.mask   Image mask
+ * 
+ * @returns {Promise<Buffer>[]} Promises of shaped images
+ */
+function shapeImage({ image, shape: { width, height, mask } }) {
+    let shaped = sharp(image).resize(width, height);
+
+    if (mask) {
+        shaped = shaped.overlayWith(mask, { cutout: true });
+    }
+
+    return shaped.toBuffer();
+}
+
+/**
+ * Compose images of glass and layers
+ * 
+ * @async
+ * 
+ * @param {Object} params Function parameters
+ * @param {Buffer} glass Glass image data
+ * @param {Buffer[]} layers Array of layers images data
+ * 
+ * @returns {Promise<Buffer>} Promise of composed image
+ */
+function composeImages({ glass, layers }) {
+    const positions = [
+        { top: 240, left: 46 }, // bottom layer
+        { top: 188, left: 46 }, // layer 2
+        { top: 115, left: 42 }, // layer 3
+        { top: 43, left: 40 }   // top layer
+    ];
+
+    const base = sharp(glass).toBuffer();
+
+    const composite = layers.reduce(
+        (input, overlay, index) =>
+            input.then( data =>
+                sharp(data).overlayWith(overlay, positions[index]).toBuffer()
+            ),
+        base
+    );
+
+    return composite;
+}
+
+/**
+ * Convert data to base64
+ * 
+ * @param {Any} data Data to convert
+ *
+ * @returns {String} Data in Base64 format
+ */
+function dataToBase64(data) {
+    const base64 = data.toString("base64");
+
+    return `data:image/png;base64,${base64}`;
+}
+
+module.exports = { 
+    getCocktail,
+    validParams,
+    getImages,
+    getShapedImages,
+    getImageShape,
+    shapeImage,
+    composeImages,
+    dataToBase64
+};
--- test/server/getCocktail.js
+++ test/server/getCocktail.js
@@ -4,20 +4,116 @@ require("chai").should();
 const request = require("supertest");
 
 const startServer = require("../../server/server");
+const {
+    validParams, getImages, 
+    getShapedImages, getImageShape, shapeImage, 
+    composeImages, dataToBase64
+} = require("../../server/controllers/cocktailController");
 
-describe("Get cocktail", () => {
+describe.only("Get cocktail", () => {
     let SERVER = {};
 
     before("Start server", () => {
         SERVER = startServer();
     });
 
-    it("should return json with status 501", () =>
-        request(SERVER)
-            .get("/api/cocktail")
-            .expect("Content-Type", /json/)
-            .expect(501)
-    );
+    describe("Valid params", () => {
+        it("should return true on valid params", () => {
+            const mocks = [
+                { layer1: "orange", layer2: "stones", layer3: "abc-def", layer4: "p" },
+                { layer1: "rotten-eggs", layer2: "st-o", layer3: "a" },
+                { layer1: "tesing", layer2: "t-c-p" },
+                { layer1: "test-case" },
+                { }
+            ];
+
+            mocks.map( mock => validParams(mock).should.be.true );
+        });
+
+        it("should return false if there are any unexpected params", () => {
+            const mocks = [
+                { layer1: "orange", layer2: "stones", layer3: "abc-def", layer4: "p", unexpected: "param" },
+                { layer1: "lemon", layer2: "sticks", layer5: "too-high" },
+                { shouldnt: "be-here" }
+            ];
+
+            mocks.map( mock => validParams(mock).should.be.false );
+        });
+
+        it("should return false if ingredients id's are invalid", () => {
+            const mocks = [
+                { layer1: "valid", layer2: "also-valid", layer3: "Even-This", layer4: "not valid" },
+                { layer1: "also-not-valid1111" },
+                { layer1: "nope-" },
+                { layer1: "///no" },
+            ];
+
+            mocks.map( mock => validParams(mock).should.be.false );
+        });
+
+        it("should return false if there are holes", () => {
+            const mocks = [
+                { layer1: "first", layer3: "third-already", layer4: "not valid" },
+                { layer2: "too-soon" },
+                { layer2: "definitely", layer4: "not" }
+            ];
+
+            mocks.map( mock => validParams(mock).should.be.false );
+        });
+    });
+
+    describe("Get images requests", () => {
+        it("should return array of promises of length n + 1 with n ingredients", () => {
+            const ingredients = [ "orange", "stone", "rotten-eggs" ];
+
+            const requests = getImages(ingredients);
+
+            requests.should.be.a("promise");
+        });
+
+        it("promises should resolve to image objects", async () => {
+            const ingredients = [ "beer", "spoiled-milk", "grass" ];
+
+            const requests = await getImages(ingredients);
+            
+            requests.should.be.an("array").and.have.lengthOf(ingredients.length + 1);
+            requests.map( req => req.should.be.instanceOf(Object)
+                                    .and.have.all.keys(
+                "AcceptRanges", "LastModified", "ContentLength", "ETag", "ContentType", "Metadata", "Body")
+            );
+            requests.map( req => req.ContentType.should.equal("image/svg+xml") );
+        });
+
+    });
+
+    describe("Get shaped images", () => {
+
+    });
+
+    describe("Get image shape", () => {
+
+    });
+
+    describe("Shape image", () => {
+
+    });
+
+    describe("Compose images", () => {
+
+    });
+
+    describe("Data to base64", () => {
+
+    });
+
+    describe("Response", () => {
+        it("should be json with status 200 when no params", () =>
+            request(SERVER)
+                .get("/api/cocktail")
+                .expect("Content-Type", /json/)
+                .expect(200)
+        );
+    });
 
     after("Shut down server", done => {
         SERVER.close(done);
