36f7e0e506e8ec6445489e6a6f75cd8c4b4ce35c
chelicerae <DenMiller45@gmail.com>
2018-06-28 13:12:16 +0300

    testing publish_actions

--- client/js/facebook-api.js
+++ client/js/facebook-api.js
@@ -22,15 +22,30 @@ function fb_share() {
 function fb_login() {
   FB.login(function(response){
     console.log('Response status: '+response.status);
-    FB.ui({
-      method: 'feed',
-      //link: 'brewtal link goes here',
-      //redirect_uri: 'google.com',
-      caption: 'Test FB API',
-      picture: 'https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&h=350',
-      description: 'This is a dog.'
-    }, function(response){});
-  }, {scope: 'publish_pages,manage_pages'});
+    if (response.authResponse) {
+        var access_token =   FB.getAuthResponse()['accessToken'];
+        FB.api('/me/photos?access_token='+access_token, 'post', { url: 'https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&h=350', access_token: access_token }, function(response) {
+            if (!response || response.error) {
+                alert('Error occured: ' + JSON.stringify(response.error));
+            } else {
+                alert('Post ID: ' + response);
+            }
+        });
+    } else {
+        console.log('User cancelled login or did not fully authorize.');
+    }
+    // FB.ui({
+    //   method: 'feed',
+    //   //link: 'brewtal link goes here',
+    //   //redirect_uri: 'google.com',
+    //   caption: 'Test FB API AAAA!!!!',
+    //   name: 'AAAA!!!!',
+    //   description: 'AAAAAA!!!!!1111',
+    //   picture: 'https://images.pexels.com/photos/356378/pexels-photo-356378.jpeg?auto=compress&cs=tinysrgb&h=350',
+    //   description: 'This is a dog.'
+    // }, function(response){});
+
+  }, {scope:'manage_pages,publish_pages,publish_to_groups'});
 }
 
 
